98420be2394a95536449630a9a42bd971d66c7a2
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-01-21 09:24:39 +0000

    update

---
+++ src/views/Home.jsx
@@ -0,0 +1,68 @@
+import { Col, Container, Row } from "react-bootstrap"
+import ExistingPosts from "../components/ExistingPosts"
+import NewPost from "../components/NewPost"
+import { useState , useEffect } from "react"
+import LeftSideBarCardProfile from "../leftSideBarHome/LeftSideBarCardProfile"
+import RightSideBar from "../rightSideBarHome/RightSideBar"
+import NavBar from "../components/NavBar";
+
+const Profile = (props) => {
+  
+  const [ posts, setPosts ] = useState([]);
+
+  const [ profile , setProfile ] = useState([])
+
+  const fetchPosts = async () => {
+    try {
+      let response = await fetch(`http://localhost:5001/post`);
+
+      if (response.ok) {
+        let data = await response.json();
+        setPosts(data.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchProfile = async () => {
+    try {
+      let response = await fetch(
+        `https://strive-linkedin.herokuapp.com/profile/619234e538541a787a13c554`);
+      if (response.ok) {
+        let data = await response.json()
+        setProfile(data)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+    fetchProfile();
+  }, []);
+
+  return (
+    <>
+    <NavBar/>
+    <Container style={{ marginTop: "100px" }}>
+      <Row>
+        <Col xs={12} md={4} lg={2} style={{ height: "100%" }}>
+          <LeftSideBarCardProfile profile={profile}/>
+        </Col>
+        <Col xs={12} md={8} lg={6} style={{ height: "100%" }}>
+          <NewPost profile={profile}  posts={posts} fetchPosts={fetchPosts} />
+          <ExistingPosts profile={profile} posts={posts} fetchPosts={fetchPosts} />
+        </Col>
+
+        <Col md={4} className="d-none d-md-block" style={{ height: "100%" }}>
+          <RightSideBar />
+        </Col>
+      </Row>
+    </Container>
+    </>
+  );
+};
+
+export default Profile;
